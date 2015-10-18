907d967ef11ce04f23931fc5681026adccf426bb
Andor Penzes <andor.penzes@gmail.com>
2015-10-19 01:28:38 +0200

    Fix typo

--- Haskell/01_ArraysAndStrings.hs
+++ Haskell/01_ArraysAndStrings.hs
@@ -79,7 +79,7 @@ replaceSpaces (c:cs) = c:replaceSpaces cs
 
 -- Exercise 1.5
 -- ============
--- Implement a method to perfirm basic string compression using the counts of
+-- Implement a method to perform basic string compression using the counts of
 -- repeated characters. For example, the string aabcccccaaa would become a
 -- a2b1c5a3. If the "compressed" string would not become smaller than the
 -- original string, your method should return the original string. You
